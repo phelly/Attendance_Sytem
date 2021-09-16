c539cc3cfe5ff3a294df08eacf40c1e7592a0462
phelly <philndungu@gmail.com>
2021-09-17 01:16:49 +0300

    s

--- attendance_system/settings.py
+++ attendance_system/settings.py
@@ -121,12 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-IMAGES_ROOT = os.path.join(STATIC_ROOT,'images')
-ATTENDANCE_ROOT =os.path.join(STATIC_ROOT,'attendance.csv')
-# STATICFILES_DIRS = (
-#     os.path.join(os.path.dirname(__file__), "images")
-# ),
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+IMAGES_ROOT = os.path.join(PROJECT_ROOT,'images')
+ATTENDANCE_ROOT =os.path.join(PROJECT_ROOT,'attendance.csv')
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
